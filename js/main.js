be56ace52f0bbc2ee0b104c62d9d6d6d907109dc
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-06 14:41:49 +0200

    assegnazione fizz, buzz e buzzfizz per i multipli creazione elementi div in pagina tramite js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 // Stampa di numeri da 1 a 100
+let displayNum;
 
 for (let i=1; i <=100; i++){
     
-// verifica della condizione se sono divisivili per 3
+// verifica della condizione se sono divisivili per 3 e 5
     if (i % 3 === 0 && i % 5 === 0){
+        displayNum = "FizzBuzz"
         console.log(i, "divisibile per 3 e 5")
     } else if (i % 3 === 0){
+        displayNum = "Fizz"
         console.log(i, "divisibile per 3")
     } else if (i % 5 === 0){
+        displayNum = "Buzz"
         console.log(i, "divisibile per 5")
     } else {
+        displayNum = i
         console.log(i, "non divisibili per 3 o 5")
     }
+
+    document.getElementById("container").innerHTML += `<div class="box"> ${displayNum} </div>`
+
         
 
 
